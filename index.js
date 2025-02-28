f8279add0ec6fa6a149318018ff7f47c7b956a14
AchmadLyraa <ahmadbayhaqi069@gmail.com>
2025-02-28 10:30:27 +0800

    Praktikum minggu ke-5, INTEGRASI FRONTEND & BACKEND (FETCH DATA)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(cors());
 
